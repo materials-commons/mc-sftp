69ca389f04bf12ed625f656f07fe246541da6232
gtarcea <gtarcea@umich.edu>
2022-04-26 15:18:04 -0400

    Small changes working through how independent the system needs to be on file transfers

--- pkg/mcsftp/handler.go
+++ pkg/mcsftp/handler.go
@@ -3,6 +3,7 @@ package mcsftp
 import (
 	"io"
 	"os"
+	"sync"
 
 	"github.com/materials-commons/gomcdb/mcmodel"
 	"github.com/materials-commons/gomcdb/store"
@@ -18,6 +19,8 @@ type MCStores struct {
 type MCHandler struct {
 	User   *mcmodel.User
 	stores *MCStores
+	mu     sync.Mutex
+	file   *os.File
 }
 
 func NewMCHandler(user *mcmodel.User, stores *MCStores) *MCHandler {
@@ -33,6 +36,31 @@ func (h *MCHandler) Fileread(r *sftp.Request) (io.ReaderAt, error) {
 }
 
 func (h *MCHandler) Filewrite(r *sftp.Request) (io.WriterAt, error) {
+	flags := r.Pflags()
+	if !flags.Write {
+		return nil, os.ErrInvalid
+	}
+
+	openFlags := os.O_RDWR
+	if flags.Trunc {
+		openFlags &= os.O_TRUNC
+	}
+
+	if flags.Creat {
+		openFlags &= os.O_CREATE
+	}
+
+	if flags.Append {
+		openFlags &= os.O_APPEND
+	}
+
+	var err error
+	h.file, err = os.OpenFile("stuff", openFlags, 0777)
+	if err != nil {
+		return nil, err
+	}
+
+	// somehow return WriterAt
 	return nil, nil
 }
 
@@ -42,4 +70,8 @@ func (h *MCHandler) Filecmd(r *sftp.Request) error {
 
 func (h *MCHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 	return nil, nil
+}
+
+func (h *MCHandler) Close() error {
+	return nil
 }
