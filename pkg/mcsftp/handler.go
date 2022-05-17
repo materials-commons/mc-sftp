b9793a2dc5552cb7bb1a0de0fd73b85fad748171
gtarcea <gtarcea@umich.edu>
2022-05-17 10:49:45 -0400

    Clean up and comments in preparation for testing of the sftp handler

--- pkg/mcsftp/handler.go
+++ pkg/mcsftp/handler.go
@@ -16,31 +16,62 @@ import (
 	"github.com/pkg/sftp"
 )
 
+// MCFile represents a single SFTP file read or write request. It handles the ReadAt, WriteAt and Close
+// interfaces for SFTP file handling.
 type MCFile struct {
-	user *mcmodel.User
+	// file is the underlying Materials Commons file that is being accessed.
 	file *mcmodel.File
-	mcmodel.FileInfo
-	dir          *mcmodel.File
-	project      *mcmodel.Project
-	stores       *mc.Stores
-	fileHandle   *os.File
-	ftpPath      string
-	projectPath  string
+
+	// dir is the underlying Materials Commons directory that the file (above) is in.
+	dir *mcmodel.File
+
+	// project is the Materials Commons project that the file is in.
+	project *mcmodel.Project
+
+	// stores are the various stores to update
+	stores *mc.Stores
+
+	// The real underlying handle to read/write the file.
+	fileHandle *os.File
+
+	// openForWrite is true when the file was opened for write. This is used in MCFile.Close() to
+	// determine if file statistics and checksum handling should be done.
 	openForWrite bool
-	hasher       hash.Hash
-	mcfsRoot     string
+
+	// hasher tracks the checksum for files that were opened for write.
+	hasher hash.Hash
+
+	// mcfsRoot is the directory path where Materials Commons files are being read from/written to.
+	mcfsRoot string
 }
 
+// mcfsHandler represents an SFTP connection. A connection is associated with a single user. That user
+// may access multiple projects. Projects are accessed by including them in the path. Each project
+// in Materials Commons has a unique project slug, which is a derived from the project name. This slug
+// is included in the path when accessing files. For example, given a project with a slug 'my-project',
+// then you would access a file by prepending my-project to the path, such as /my-project/dir1/file.txt.
+// In this case the user is access file.txt in dir1 in project with the project slug my-project. The
+// path to the file.txt file would be /dir1/file.txt in the project with slug my-project. A few things
+// to note:
+//
+// 1. All the SFTP callbacks have to handle manipulating the path get the project slug, and to remove
+//    it from the path so specify the underlying Materials Commons path.
+//
+// 2. Because a user can access many files, which could be in different projects, we don't want to
+//    continuously look up projects. The mcfsHandler caches projects that were already looked up
+//    so they can be quickly returned these are cached by project slug. It also caches failed projects
+//    either because the project-slug didn't exist or the user didn't have access to the project.
+//
 type mcfsHandler struct {
-	user     *mcmodel.User
-	stores   *mc.Stores
+	// user is the Materials Commons user for this SFTP session.
+	user   *mcmodel.User
+	stores *mc.Stores
+
+	// mcfsRoot is the directory path where Materials Commons files are being read from/written to.
 	mcfsRoot string
 
 	// Protects files, projects, and projectsWithoutAccess
 	mu sync.Mutex
-
-	// Tracks all the files the user has opened. The key is the path with the project slug.
-	files map[string]*MCFile
 
 	// Tracks all the projects the user has accessed that they also have rights to.
 	// The key is the project slug.
@@ -55,13 +86,13 @@ func NewMCFSHandler(user *mcmodel.User, stores *mc.Stores, mcfsRoot string) *mcf
 	return &mcfsHandler{
 		user:                  user,
 		stores:                stores,
-		files:                 make(map[string]*MCFile),
 		projects:              make(map[string]*mcmodel.Project),
 		projectsWithoutAccess: make(map[string]bool),
 		mcfsRoot:              mcfsRoot,
 	}
 }
 
+// Fileread sets up read access to an existing Materials Commons file.
 func (h *mcfsHandler) Fileread(r *sftp.Request) (io.ReaderAt, error) {
 	flags := r.Pflags()
 	if !flags.Read {
@@ -81,67 +112,50 @@ func (h *mcfsHandler) Fileread(r *sftp.Request) (io.ReaderAt, error) {
 		return nil, os.ErrNotExist
 	}
 
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.files[r.Filepath] = mcFile
-
 	return mcFile, nil
 }
 
-func getPathFromRequest(r *sftp.Request) string {
-	projectSlug := mc.GetProjectSlugFromPath(r.Filepath)
-	return mc.RemoveProjectSlugFromPath(r.Filepath, projectSlug)
-}
-
+// Filewrite sets up a file for writing. It create a file or new file version in Materials Commons
+// as well as the underlying real physical file to write to.
 func (h *mcfsHandler) Filewrite(r *sftp.Request) (io.WriterAt, error) {
 	flags := r.Pflags()
 	if !flags.Write {
+		// Pathological case, Filewrite should always have the flags.Write set to true.
 		return nil, os.ErrInvalid
 	}
 
+	// Set up the initial SFTP request file state.
 	mcFile, err := h.createMCFileFromRequest(r)
 	if err != nil {
 		return nil, os.ErrNotExist
 	}
 
-	fileName := filepath.Base(mcFile.projectPath)
+	// Create the Materials Commons file. This handles version creation.
+	fileName := filepath.Base(r.Filepath)
 	mcFile.file, err = h.stores.FileStore.CreateFile(fileName, mcFile.project.ID, mcFile.dir.ID, h.user.ID, mc.GetMimeType(fileName))
 	if err != nil {
 		return nil, os.ErrNotExist
 	}
 
-	mcFile.FileInfo = mcFile.file.ToFileInfo()
-
-	openFlags := os.O_RDWR
-	if flags.Trunc {
-		openFlags &= os.O_TRUNC
-	}
-
-	if flags.Creat {
-		openFlags &= os.O_CREATE
-	}
-
-	if flags.Append {
-		openFlags &= os.O_APPEND
-	}
+	// The flags don't matter, we will always open the file for create. Because files are versioned
+	// in Materials Commons there is no appending, truncating or overwriting of files.
+	openFlags := os.O_RDWR & os.O_CREATE
 
 	if mcFile.fileHandle, err = os.OpenFile(mcFile.file.ToUnderlyingFilePath(h.mcfsRoot), openFlags, 0777); err != nil {
 		return nil, err
 	}
 
+	// Since this file was opened for writing we need to track its checksum, and for MCFile.Close() let
+	// it know whether it needs to update statistics about the file (only when openForWrite is true).
 	mcFile.openForWrite = true
 	mcFile.hasher = md5.New()
-
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.files[r.Filepath] = mcFile
 
 	return mcFile, nil
 }
 
-// mcfileSetup will setup the MCFile that is used for reading/writing of files. It
+// createMCFileFromRequest will create a new MCFile that is used for reading/writing of files. It
 // performs the actions of determining the project, setting up paths, and similar
-// setup items needed to create a MCFile regardless of
+// setup items needed to create a MCFile.
 func (h *mcfsHandler) createMCFileFromRequest(r *sftp.Request) (*MCFile, error) {
 	project, err := h.getProject(r)
 	if err != nil {
@@ -156,15 +170,14 @@ func (h *mcfsHandler) createMCFileFromRequest(r *sftp.Request) (*MCFile, error) 
 	}
 
 	return &MCFile{
-		user:        h.user,
-		project:     project,
-		dir:         dir,
-		ftpPath:     r.Filepath,
-		projectPath: path,
-		stores:      h.stores,
+		project: project,
+		dir:     dir,
+		stores:  h.stores,
 	}, nil
 }
 
+// Filecmd supports various SFTP commands that manipulate a file and/or filesystem. It only supports
+// Mkdir for directory creation. Deletes, renames, setting permissions, etc... are not supported.
 func (h *mcfsHandler) Filecmd(r *sftp.Request) error {
 	project, err := h.getProject(r)
 	if err != nil {
@@ -191,6 +204,8 @@ func (h *mcfsHandler) Filecmd(r *sftp.Request) error {
 	}
 }
 
+// Filelist handles the different SFTP file list type commands. We only support List (directory listing)
+// and Stat. Things like Readlink don't make sense for Materials Commons.
 func (h *mcfsHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 	path := mc.RemoveProjectSlugFromPath(r.Filepath, getPathFromRequest(r))
 	project, err := h.getProject(r)
@@ -221,9 +236,9 @@ func (h *mcfsHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 	default:
 		return nil, fmt.Errorf("unsupport command: '%s'", r.Method)
 	}
-	return nil, nil
 }
 
+// Realpath always returns the absolute path including the project slug.
 func (h *mcfsHandler) Realpath(p string) string {
 	p = filepath.ToSlash(filepath.Clean(p))
 	if !filepath.IsAbs(p) {
@@ -233,6 +248,8 @@ func (h *mcfsHandler) Realpath(p string) string {
 	return p
 }
 
+// Lstat returns a single entry array containing the requested file, assuming it exists. It
+// returns os.ErrNotExist if it doesn't exist.
 func (h *mcfsHandler) Lstat(r *sftp.Request) (sftp.ListerAt, error) {
 	path := mc.RemoveProjectSlugFromPath(r.Filepath, getPathFromRequest(r))
 	project, err := h.getProject(r)
@@ -248,6 +265,7 @@ func (h *mcfsHandler) Lstat(r *sftp.Request) (sftp.ListerAt, error) {
 
 type listerat []os.FileInfo
 
+// ListAt verifies that the particular index exists in the files array.
 func (f listerat) ListAt(files []os.FileInfo, offset int64) (int, error) {
 	var n int
 	if offset >= int64(len(f)) {
@@ -258,40 +276,6 @@ func (f listerat) ListAt(files []os.FileInfo, offset int64) (int, error) {
 		return n, io.EOF
 	}
 	return n, nil
-}
-
-func (h *mcfsHandler) getProject(r *sftp.Request) (*mcmodel.Project, error) {
-	var (
-		ok      bool
-		project *mcmodel.Project
-		err     error
-	)
-
-	h.mu.Lock()
-	defer h.mu.Unlock()
-
-	projectSlug := mc.GetProjectSlugFromPath(r.Filepath)
-	if project, ok = h.projects[projectSlug]; ok {
-		return project, nil
-	}
-
-	if _, ok := h.projectsWithoutAccess[projectSlug]; ok {
-		return nil, fmt.Errorf("no such project: %s", projectSlug)
-	}
-
-	if project, err = h.stores.ProjectStore.GetProjectBySlug(projectSlug); err != nil {
-		h.projectsWithoutAccess[projectSlug] = true
-		return nil, err
-	}
-
-	if !h.stores.ProjectStore.UserCanAccessProject(h.user.ID, project.ID) {
-		h.projectsWithoutAccess[projectSlug] = true
-		return nil, fmt.Errorf("no such project: %s", projectSlug)
-	}
-
-	h.projects[projectSlug] = project
-
-	return project, err
 }
 
 // Close handles updating the metadata on a file stored in Materials Commons as well as
@@ -319,7 +303,7 @@ func (f *MCFile) Close() error {
 	}
 
 	// If we are here then the file was open for write, so lets update the metadata
-	// that Materials Commons is tracker.
+	// that Materials Commons is tracking.
 
 	finfo, err := f.fileHandle.Stat()
 	if err != nil {
@@ -337,6 +321,53 @@ func (f *MCFile) Close() error {
 	}
 
 	return nil
+}
+
+// getProject retrieves the project from the path. The r.Filepath contains the project slug as
+// a part of the path. This method strips that out. The mcfsHandler has two caches for projects
+// the first mcfsHandler.projects is a cache of already loaded projects, indexed by the slug. The
+// second is mcfsHandler.projectsWithoutAccess which is a cache of booleans indexed by the project
+// slug for project slugs that either don't exist or that the user doesn't have access to. Only
+// if the slug isn't found in either of these caches is an attempt to look it up (and if the
+// lookup is successful also check access) done. The lookup will fill out the appropriate
+// project cache (mcfsHandler.projects or mcfsHandler.projectsWithoutAccess).
+func (h *mcfsHandler) getProject(r *sftp.Request) (*mcmodel.Project, error) {
+	var (
+		ok      bool
+		project *mcmodel.Project
+		err     error
+	)
+
+	// Protect access to the two project caches.
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	projectSlug := mc.GetProjectSlugFromPath(r.Filepath)
+	if project, ok = h.projects[projectSlug]; ok {
+		return project, nil
+	}
+
+	if _, ok := h.projectsWithoutAccess[projectSlug]; ok {
+		return nil, fmt.Errorf("no such project: %s", projectSlug)
+	}
+
+	// If we are here then we haven't seen this project slug before.
+	if project, err = h.stores.ProjectStore.GetProjectBySlug(projectSlug); err != nil {
+		// Can't find it so treat as no access
+		h.projectsWithoutAccess[projectSlug] = true
+		return nil, err
+	}
+
+	if !h.stores.ProjectStore.UserCanAccessProject(h.user.ID, project.ID) {
+		// Found it but user doesn't have access to it.
+		h.projectsWithoutAccess[projectSlug] = true
+		return nil, fmt.Errorf("no such project: %s", projectSlug)
+	}
+
+	// Found the project and user has access so put in the projects cache.
+	h.projects[projectSlug] = project
+
+	return project, err
 }
 
 // WriteAt takes care of writing to the file and updating the hasher that is
@@ -370,6 +401,14 @@ func (f *MCFile) ReadAt(b []byte, offset int64) (int, error) {
 	return n, err
 }
 
+// isOpenForRead returns true if the file was opened for read. It exists for readability purposes.
 func (f *MCFile) isOpenForRead() bool {
 	return f.openForWrite == false
+}
+
+// getPathFromRequest will get the path to the file from the request after it removes the
+// project slug.
+func getPathFromRequest(r *sftp.Request) string {
+	projectSlug := mc.GetProjectSlugFromPath(r.Filepath)
+	return mc.RemoveProjectSlugFromPath(r.Filepath, projectSlug)
 }
