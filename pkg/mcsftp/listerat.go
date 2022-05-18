1366793acd938cf592a9b00a70302e69164faf4e
gtarcea <gtarcea@umich.edu>
2022-05-18 10:24:28 -0400

    Add comments

--- pkg/mcsftp/listerat.go
+++ pkg/mcsftp/listerat.go
@@ -9,14 +9,18 @@ type listerat []os.FileInfo
 
 // ListAt verifies that the particular index exists in the files array.
 func (f listerat) ListAt(files []os.FileInfo, offset int64) (int, error) {
-	var n int
 	if offset >= int64(len(f)) {
+		// offset is greater than the number of entries in f, signal this by return error io.EOF
 		return 0, io.EOF
 	}
-	n = copy(files, f[offset:])
+
+	n := copy(files, f[offset:])
 	if n < len(files) {
+		// Number copied all the remaining entries in f, so nothing left
+		// to copy after this. So return io.EOF to signal this
 		return n, io.EOF
 	}
 
+	// Copied entries, but some left, so don't return EOF.
 	return n, nil
 }
